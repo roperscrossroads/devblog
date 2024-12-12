d0a70904c199045236d3d648a1fea9ae27ec2598
Adam Roper <adam.r.roper@gmail.com>
2024-12-12 18:50:45 -0500

    fix: comment out Comments for now

--- layouts/PostLayout.tsx
+++ layouts/PostLayout.tsx
@@ -82,14 +82,14 @@ export default function PostLayout({ content, authorDetails, next, prev, childre
               {/* <div className="pb-6 pt-6 text-sm text-gray-700 dark:text-gray-300">
                 <Link href={editUrl(filePath)}>View this article on GitHub</Link>
               </div> */}
-              {siteMetadata.comments && (
+              {/* {siteMetadata.comments && (
                 <div
                   className="pb-6 pt-6 text-center text-gray-700 dark:text-gray-300"
                   id="comment"
                 >
                   <Comments slug={slug} />
                 </div>
-              )}
+              )} */}
             </div>
             <footer>
               <div className="divide-gray-200 text-sm font-medium leading-5 xl:col-start-1 xl:row-start-2 xl:divide-y dark:divide-gray-700">
